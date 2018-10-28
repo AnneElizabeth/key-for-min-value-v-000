04ffe01ba763ebbb5d3fdfeb516c42666e398640
Anne Edmondson <edmondson.anne@gmail.com>
2018-10-28 04:01:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,18 @@
 def key_for_min_value(hash)
+  #nil is a placeholder for the variable
   lowest_key = nil
-  lowest_value = Float::INFINITY
+  
+  #lowest_value is used so you can compare values 
+  #base value to use in the comparison, 0 is just a place to start
+  
+  lowest_value = 0
+  
   hash.each do |k, v|
-    if v < lowest_value
+    #lowest_value = 0 at beginning of loop
+    if lowest_value == 0 || v < lowest_value
+      
+      #set lowest_value equal to the first value in the hash so you can compare to next v in the iteration
+      
       lowest_value = v
       lowest_key = k
     end
